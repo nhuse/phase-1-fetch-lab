2bf08aabba9373da5bc93cbf653ea75c91c031be
nhuse <nathan.huse@yahoo.com>
2021-07-06 11:20:05 -0500

    commit tree 1739dddb97a4be364585178ff1dedca5d23f2d2d

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
+const { default: fetch } = require("node-fetch");
+
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+  fetch('https://anapioficeandfire.com/api/books').then(function(response){
+    return response.json();
+  }).then(json => renderBooks(json));
+
+  return fetch('https://anapioficeandfire.com/api/books');
 }
 
 function renderBooks(books) {
@@ -15,3 +21,4 @@ function renderBooks(books) {
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks();
 });
+
